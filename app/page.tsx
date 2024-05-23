dd4f37e5053ce6ff682257a62401049bbfe65d43
Tiefu Zhu <tiefu.zhu@gmail.com>
2024-05-23 16:02:42 +1000

    customized home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,47 @@
-import { auth } from "@clerk/nextjs/server";
+import prisma from '@/prisma/client'
+import RequestsTable from '@/components/requests-table'
+import { columns } from '@/components/columns'
+import { getUsernames } from '@/utils/getUsernames'
+import AddRequestButton from '@/app/requests/_components/add-request-button'
+import { auth } from '@clerk/nextjs/server'
+
+async function getUserRequests() {
+    const { userId } = auth()
+    try {
+        const requests = await prisma.request.findMany({
+            where: { requesterId: userId as string },
+            orderBy: { createdAt: 'desc' },
+        })
+        return requests
+    } catch (error) {
+        console.error(error)
+        return []
+    }
+}
+
+async function loader() {
+    const requests = await getUserRequests()
+    const userIds = requests.map((req) => req.requesterId)
+    const userMap = await getUsernames(userIds)
+
+    const enrichedRequests = requests.map((request) => ({
+        ...request,
+        username: userMap.find((user) => user.userId === request.requesterId)!.username,
+    }))
+
+    return enrichedRequests
+}
 
 export default async function Home() {
-    const { userId } = auth();
-    return <main>hello {userId}</main>;
+    const enrichedRequests = await loader()
+
+    return (
+        <div className='space-y-6 p-8'>
+            <div className='flex justify-between items-end'>
+                <div className='flex flex-1 justify-center text-2xl font-bold'>Your Requests</div>
+                <AddRequestButton />
+            </div>
+            {enrichedRequests && <RequestsTable columns={columns} data={enrichedRequests} />}
+        </div>
+    )
 }
---
+++ components/editable-cell.tsx
@@ -0,0 +1,6 @@
+import { Input } from '@/components/ui/input'
+type Props = {}
+
+export function EditableCell({}: Props) {
+    return <Input />
+}
